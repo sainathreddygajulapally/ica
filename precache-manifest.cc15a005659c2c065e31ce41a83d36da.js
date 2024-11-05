cba3ac23bda93171581697850f026190dfad7dea
Sai gajulapally <saigajulapally@Sais-MacBook-Pro.local>
2024-11-05 13:32:14 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.cc15a005659c2c065e31ce41a83d36da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a568ba298cb11d2fdcdc6cbc827e3eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50430f7422da86cb44a3",
+    "url": "/static/css/main.61e179f8.chunk.css"
+  },
+  {
+    "revision": "aa128a4d6742449650f3",
+    "url": "/static/js/2.9515a515.chunk.js"
+  },
+  {
+    "revision": "cd136a8ab8e7f5fd00e8",
+    "url": "/static/js/3.3ef9971d.chunk.js"
+  },
+  {
+    "revision": "50430f7422da86cb44a3",
+    "url": "/static/js/main.f5d0f978.chunk.js"
+  },
+  {
+    "revision": "f48c669cdd8d874b14c5",
+    "url": "/static/js/runtime~main.ec6a9e12.js"
+  },
+  {
+    "revision": "66549c561628761af2103fe506df7cfa",
+    "url": "/static/media/IcarusTaxGroupLogo.66549c56.png"
+  }
+]);
